f4d022bc09f79e7e2e46d145abdc1acba8618c70
Toh <jagraphob.j@gmail.com>
2025-05-18 10:24:05 +1200

    setup picocss, layout and nav

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="container">
+    <ul>
+      <li><strong>Tools</strong></li>
+    </ul>
+    <ul>
+      <li><a href="#">About</a></li>
+    </ul>
+  </nav>
+  <main class="container">
+      <slot></slot>
+  </main>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-
+  css: ["@picocss/pico"],
   nitro: {
     preset: "cloudflare-pages",
 
