03724b5ca2ca0d58086edeb1ad00cf8a4e6dc289
Toh <jagraphob.j@gmail.com>
2025-05-27 14:42:03 +1200

    layouts

---
+++ components/nav-bar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav class="container">
+    <ul>
+      <li>
+        <NuxtLink to="/"><h1>Tool</h1></NuxtLink>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <NuxtLink :to="{ name: 'planning-poker' }">Poker</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+  a, a:visited, a:hover, a:active {
+  color: inherit;
+  text-decoration: none;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,5 @@
 <template>
-  <nav class="container">
-    <ul>
-      <li><strong>Tools</strong></li>
-    </ul>
-    <ul>
-      <li><a href="#">About</a></li>
-    </ul>
-  </nav>
+  <NavBar />
   <main class="container">
       <slot></slot>
   </main>
